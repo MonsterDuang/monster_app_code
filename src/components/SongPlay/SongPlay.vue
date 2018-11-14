39c31e9492dd02a734518836fc88be23ec92842f
luozhiyong <luozhiyong@163.com>
2018-11-14 14:05:15 +0800

    优化不能自动滚动的歌词

--- src/components/SongPlay/SongPlay.vue
+++ src/components/SongPlay/SongPlay.vue
@@ -158,9 +158,10 @@ export default {
       }
     },
     onPan (value) {
-      if (value.additionalEvent === 'panleft' && value.isFinal === true) {
+      console.log(value)
+      if (value.additionalEvent === 'panleft' && value.isFinal === true && value.deltaX < -100) {
         this.$store.dispatch('changeNextSong')
-      } else if (value.additionalEvent === 'panright' && value.isFinal === true) {
+      } else if (value.additionalEvent === 'panright' && value.isFinal === true && value.deltaX > 100) {
         this.$store.dispatch('changeBeforeSong')
       }
     },
